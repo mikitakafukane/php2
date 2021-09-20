63fd2fe26631f2cdb2ae4a062460d1e70d5d912b
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-09-20 23:47:19 +0900

    finish28

---
+++ 3-3.php
@@ -0,0 +1,15 @@
+<?php
+// $black = '黒';
+// $white = '白';
+// $red = '赤';
+
+// $color[0] = '黒';
+// $color[1] = '白';
+// $color[2] = '赤';
+
+//配列を一気に指定する
+$color = ['黒', '白', '赤'];
+
+$number = 1;
+
+echo $color[$number];
