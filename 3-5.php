2010755c700a0e8612b46d7f47fe8b7861b6276e
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-09-22 14:34:02 +0900

    finish30

---
+++ 3-5.php
@@ -0,0 +1,21 @@
+<?php
+// $pref['hokkaido'] = '北海道';
+// $pref['aomori'] = '青森県';
+// $pref['iwate'] = '岩手県';
+
+// $pref = [
+//   'hokkaido' => '北海道',
+//   'aomori' => '青森県',
+//   'iwate' => '岩手県'
+// ];
+
+
+$pref ['hokkaido'] = [
+  '赤平市',
+  '旭川市',
+  '芦別市'
+];
+
+// $mypref = 'iwate';
+
+echo $pref['hokkaido'][1];
