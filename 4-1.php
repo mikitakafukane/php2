3c0c303de9f9a7b797c3b68df8ea8399890872ed
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-09-30 11:21:53 +0900

    finish38

---
+++ 4-1.php
@@ -0,0 +1,31 @@
+<?php
+//連想配列を受け取って、リストにして出力する
+function make_list($list) {
+  foreach ($list as $key => $value){
+    echo '・', $key, ':', $value, "\n";
+  }
+}
+
+$pref = [
+  'hokkaido' => '北海道',
+  'aomori' => '青森県',
+  'iwate' => '岩手県'
+];
+
+make_list($pref);
+
+// foreach ($pref as $pref_code => $pref_name){
+//   echo '・', $pref_code, ':', $pref_name, "\n";
+// }
+
+$color = [
+  'red' => '赤',
+  'blue' => '青',
+  'black' => '黒'
+];
+
+make_list($color);
+
+// foreach ($color as $color_code => $color_name){
+//   echo '・', $color_code, ':', $color_name, "\n";
+// }
